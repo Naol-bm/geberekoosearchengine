52f51a747d8483ae80e7111a00c35b073a2f5698
naolbm <profesornaoltena@gmail.com>
2024-06-04 08:12:03 +0300

    acd

--- app/search/details/page.jsx
+++ app/search/details/page.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function page() {
-  return <div className=""></div>;
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <img
+        src="https://i.imgur.com/NRVWJH0.png"
+        className="h-screen w-screen"
+        alt=""
+      />
+    </div>
+  );
 }
